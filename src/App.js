edf87aeff7644e0958d05aa406e28201e8804b32
PashkaKra <pav892@yandex.ru>
2022-10-03 05:03:18 +0300

    Компонента Round1 исправлена для возможности повторного использования; исправлен баг с цветовой подсветкой нот

--- src/App.js
+++ src/App.js
@@ -3,15 +3,57 @@ import logo from './logo.svg';
 import {Routes, Route, Link} from 'react-router-dom';
 import Menu from './pages/Menu';
 import Round1 from './pages/Round1';
-import Round2 from './pages/Round2';
+//import Round2 from './pages/Round2';
 import Round3 from './pages/Round3';
 import Playercard from './components/Playercard/Playercard';
 import './App.css';
 
+import r1_note1 from './sounds/round1/kat1note1.mp3';
+import r1_note2 from './sounds/round1/kat1note2.mp3';
+import r1_note3 from './sounds/round1/kat1note3.mp3';                                                                                          
+import r1_note4 from './sounds/round1/kat1note4.mp3';
+import r1_note5 from './sounds/round1/kat2note1.mp3';
+import r1_note6 from './sounds/round1/kat2note2.mp3';
+import r1_note7 from './sounds/round1/kat2note3.mp3';
+import r1_note8 from './sounds/round1/kat2note4.mp3';
+import r1_note9 from './sounds/round1/kat3note1.mp3';
+import r1_note10 from './sounds/round1/kat3note2.mp3';
+import r1_note11 from './sounds/round1/kat3note3.mp3';
+import r1_note12 from './sounds/round1/kat3note4.mp3';
+import r1_note13 from './sounds/round1/kat4note1.mp3';
+import r1_note14 from './sounds/round1/kat4note2.mp3';
+import r1_note15 from './sounds/round1/kat4note3.mp3';
+import r1_note16 from './sounds/round1/kat4note4.mp3';
+
+import r2_note1 from './sounds/round2/kat1note1.mp3';
+import r2_note2 from './sounds/round2/kat1note2.mp3';
+import r2_note3 from './sounds/round2/kat1note3.mp3';
+import r2_note4 from './sounds/round2/kat1note4.mp3';
+import r2_note5 from './sounds/round2/kat2note1.mp3';
+import r2_note6 from './sounds/round2/kat2note2.mp3';
+import r2_note7 from './sounds/round2/kat2note3.mp3';
+import r2_note8 from './sounds/round2/kat2note4.mp3';
+import r2_note9 from './sounds/round2/kat3note1.mp3';
+import r2_note10 from './sounds/round2/kat3note2.mp3';
+import r2_note11 from './sounds/round2/kat3note3.mp3';
+import r2_note12 from './sounds/round2/kat3note4.mp3';
+import r2_note13 from './sounds/round2/kat4note1.mp3';
+import r2_note14 from './sounds/round2/kat4note2.mp3';
+import r2_note15 from './sounds/round2/kat4note3.mp3';
+import r2_note16 from './sounds/round2/kat4note4.mp3';
+
+
 const App = () => {
     const [pull, setPull] = useState(0);
     const PlayerName = ["Игрок1", "Игрок2", "Игрок3"];
-
+    const point1 = [20, 25, 30, 50, 0]; 
+    const point2 = [25, 50, 75, 100, 0];
+    const category1 = ["Музыка школьных дискотек", "Наши 2000-е", "Саундтреки из сериалов", "Мировые хиты"];
+    const category2 = ["Зарубежный поп", "Зарубежный рок", "Русский поп", "Русский рок"];
+    const notesRound1 = [r1_note1, r1_note2, r1_note3, r1_note4, r1_note5, r1_note6, r1_note7, r1_note8,
+        r1_note9, r1_note10, r1_note11, r1_note12, r1_note13, r1_note14, r1_note15, r1_note16];
+    const notesRound2 = [r2_note1, r2_note2, r2_note3, r2_note4, r2_note5, r2_note6, r2_note7, r2_note8,
+        r2_note9, r2_note10, r2_note11, r2_note12, r2_note13, r2_note14, r2_note15, r2_note16];
     return (
         <div>
              <header>
@@ -22,8 +64,8 @@ const App = () => {
             </header>
             <Routes>
                 <Route path="/" element={<Menu/>}/>
-                <Route path="/round1" element={<Round1 onChange={(coin) => setPull(coin)}/>}/>
-                <Route path="/round2" element={<Round2 onChange={(coin) => setPull(coin)}/>}/>
+                <Route path="/round1" element={<Round1 round="1" note={notesRound1} category={category1} onChange={(i) => setPull(point1[i])}/>}/>
+                <Route path="/round2" element={<Round1 round="2" note={notesRound2} category={category2} onChange={(i) => setPull(point2[i])}/>}/>
                 <Route path="/round3" element={<Round3/>}/>
             </Routes>
             <div className="Playercard">                                                            
--- src/pages/Round1.js
+++ src/pages/Round1.js
@@ -1,4 +1,4 @@
-import {useState, useRef} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import {Link} from 'react-router-dom';
 import React from 'react'; 
 import './Table.css';
@@ -7,35 +7,27 @@ import './Table.css';
 import note_img from './note.svg';
 import red_note_img from './rednote.svg';
 import green_note_img from './greennote.svg';
-//import playerphoto1 from '../components/Playercard/player1.jpg';
-//import playerphoto2 from '../components/Playercard/player2.jpg';
-//import playerphoto3 from '../components/Playercard/player3.jpg';
-import note1 from '../sounds/round1/kat1note1.mp3';
-import note2 from '../sounds/round1/kat1note2.mp3';
-import note3 from '../sounds/round1/kat1note3.mp3';
-import note4 from '../sounds/round1/kat1note4.mp3';
-import note5 from '../sounds/round1/kat2note1.mp3';
-import note6 from '../sounds/round1/kat2note2.mp3';
-import note7 from '../sounds/round1/kat2note3.mp3';
-import note8 from '../sounds/round1/kat2note4.mp3';
-import note9 from '../sounds/round1/kat3note1.mp3';
-import note10 from '../sounds/round1/kat3note2.mp3';
-import note11 from '../sounds/round1/kat3note3.mp3';
-import note12 from '../sounds/round1/kat3note4.mp3';
-import note13 from '../sounds/round1/kat4note1.mp3';
-import note14 from '../sounds/round1/kat4note2.mp3';
-import note15 from '../sounds/round1/kat4note3.mp3';
-import note16 from '../sounds/round1/kat4note4.mp3';
 
 
-const Round1 = ({onChange}) => {
-    const [state, setState] = useState(false);
-    const [img_note, setColorNote] = useState([
-        note_img, note_img, note_img, note_img,
-        note_img, note_img, note_img, note_img,
-        note_img, note_img, note_img, note_img,
-        note_img, note_img, note_img, note_img
-    ]);
+const Round1 = (props) => {
+    //const [state, setState] = useState([]);
+    const [img_note, setColorNote] = useState([]);
+    useEffect(() => {
+        setColorNote([                                                                                              
+            note_img, note_img, note_img, note_img,
+            note_img, note_img, note_img, note_img,
+            note_img, note_img, note_img, note_img,
+            note_img, note_img, note_img, note_img
+        ]);
+        /*setState([
+            false, false, false, false,
+            false, false, false, false,
+            false, false, false, false,
+            false, false, false, false
+        ]);*/
+    }, [props.round]);  
+    
+
     const noteRef1 = useRef();
     const noteRef2 = useRef();
     const noteRef3 = useRef();
@@ -52,30 +44,29 @@ const Round1 = ({onChange}) => {
     const noteRef14 = useRef();
     const noteRef15 = useRef();
     const noteRef16 = useRef();
-    let point = [20, 25, 30, 50];
-    const category = ["Музыка школьных дискотек", "Наши 2000-е", "Саундтреки из сериалов", "Мировые хиты"];
 
     const audplay = (ref, i, q) => {
-        if(!state){            
+        //if(!state[i]){
+        if((img_note[q] == note_img) || (img_note[q] == red_note_img)){
             ref.current.play();
-            setState(true);
-            onChange(point[i]);
+            //setState(true);
+            props.onChange(i);
             setColorNote([...img_note.slice(0, q), green_note_img, ...img_note.slice(q + 1)]);
         }
         else{
             ref.current.pause();
-            setState(false);
-            onChange(0);
+            //setState(false);
+            props.onChange(4);
             setColorNote([...img_note.slice(0, q), red_note_img, ...img_note.slice(q + 1)]);
         }
     }
 
     return(
         <div className="NoteTable">
-            <Link to="/">playerphoto1</Link>
+            <Link to="/">home</Link>
             <Link to="/round2">next round</Link>
             <div>
-                <div className="category"><h2>{category[0]}</h2></div>
+                <div className="category"><h2>{props.category[0]}</h2></div>
                 <div className="notewrapper">
                     <img src={img_note[0]} onClick={() => audplay(noteRef1, 0, 0)} alt="note" className="note1"/>
                     <img src={img_note[1]} onClick={() => audplay(noteRef2, 1, 1)} alt="note" className="note2"/>
@@ -85,7 +76,7 @@ const Round1 = ({onChange}) => {
             </div>
 
             <div>
-                <div className="category"><h2>{category[1]}</h2></div>
+                <div className="category"><h2>{props.category[1]}</h2></div>
                 <div className="notewrapper">
                     <img src={img_note[4]} onClick={() => audplay(noteRef5, 0, 4)} alt="note" className="note1"/>                
                     <img src={img_note[5]} onClick={() => audplay(noteRef6, 1, 5)} alt="note" className="note2"/>                
@@ -95,7 +86,7 @@ const Round1 = ({onChange}) => {
             </div>
 
             <div>
-                <div className="category"><h2>{category[2]}</h2></div>
+                <div className="category"><h2>{props.category[2]}</h2></div>
                 <div className="notewrapper">
                     <img src={img_note[8]} onClick={() => audplay(noteRef9, 0, 8)} alt="note" className="note1"/>                
                     <img src={img_note[9]} onClick={() => audplay(noteRef10, 1, 9)} alt="note" className="note2"/>                
@@ -105,7 +96,7 @@ const Round1 = ({onChange}) => {
             </div>
                                                                                                                                  
             <div>
-                <div className="category"><h2>{category[3]}</h2></div>
+                <div className="category"><h2>{props.category[3]}</h2></div>
                 <div className="notewrapper">
                     <img src={img_note[12]} onClick={() => audplay(noteRef13, 0, 12)} alt="note" className="note1"/>                
                     <img src={img_note[13]} onClick={() => audplay(noteRef14, 1, 13)} alt="note" className="note2"/>                
@@ -114,22 +105,22 @@ const Round1 = ({onChange}) => {
                 </div>
             </div>
 
-            <audio ref={noteRef1} src={note1}/>
-            <audio ref={noteRef2} src={note2}/>
-            <audio ref={noteRef3} src={note3}/>                                                                                  
-            <audio ref={noteRef4} src={note4}/>
-            <audio ref={noteRef5} src={note5}/>                                                                                  
-            <audio ref={noteRef6} src={note6}/>                                                                                  
-            <audio ref={noteRef7} src={note7}/>                                                                                  
-            <audio ref={noteRef8} src={note8}/>
-            <audio ref={noteRef9} src={note9}/>                                                                                  
-            <audio ref={noteRef10} src={note10}/>                                                                                  
-            <audio ref={noteRef11} src={note11}/>                                                                                  
-            <audio ref={noteRef12} src={note12}/>                                                                                  
-            <audio ref={noteRef13} src={note13}/>                                                                                  
-            <audio ref={noteRef14} src={note14}/>                                                                                  
-            <audio ref={noteRef15} src={note15}/>                                                                                  
-            <audio ref={noteRef16} src={note16}/>
+            <audio ref={noteRef1} src={props.note[0]}/>
+            <audio ref={noteRef2} src={props.note[1]}/>
+            <audio ref={noteRef3} src={props.note[2]}/>                                                                                  
+            <audio ref={noteRef4} src={props.note[3]}/>
+            <audio ref={noteRef5} src={props.note[4]}/>                                                                                  
+            <audio ref={noteRef6} src={props.note[5]}/>                                                                                  
+            <audio ref={noteRef7} src={props.note[6]}/>                                                                                  
+            <audio ref={noteRef8} src={props.note[7]}/>
+            <audio ref={noteRef9} src={props.note[8]}/>                                                                                  
+            <audio ref={noteRef10} src={props.note[9]}/>                                                                                  
+            <audio ref={noteRef11} src={props.note[10]}/>                                                                                  
+            <audio ref={noteRef12} src={props.note[11]}/>                                                                                  
+            <audio ref={noteRef13} src={props.note[12]}/>                                                                                  
+            <audio ref={noteRef14} src={props.note[13]}/>                                                                                  
+            <audio ref={noteRef15} src={props.note[14]}/>                                                                                  
+            <audio ref={noteRef16} src={props.note[15]}/>
         </div>
     );
 }
