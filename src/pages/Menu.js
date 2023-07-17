475ce177cd772cdce8a803702f5e8149d6382b2d
PashkaKra <pav892@yandex.ru>
2023-07-17 07:43:48 +0300

    изменил цвет сыгранных нот, подправил размещение категорий

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom';
 const Menu = () => {
     return(
         <div className="mainbox">                                                                                   
-            <Link to="/round1" className="RoundLink"><div>1</div><div>Раунд</div></Link>
-            <Link to="/round2" className="RoundLink"><div>2</div><div>Раунд</div></Link>
-            <Link to="/round3" className="RoundLink"><div>3</div><div>Раунд</div></Link>
-            <Link to="/round4" className="RoundLink"><div>4</div><div>Раунд</div></Link>
+            <Link to="/round1" className="RoundLink"><div>1-й<br/>Раунд</div></Link>
+            <Link to="/round2" className="RoundLink"><div>2-й<br/>Раунд</div></Link>
+            <Link to="/round3" className="RoundLink"><div>3-й<br/>Раунд</div></Link>
+            <Link to="/round4" className="RoundLink"><div>4-й<br/>Раунд</div></Link>
         </div>
 
     );
